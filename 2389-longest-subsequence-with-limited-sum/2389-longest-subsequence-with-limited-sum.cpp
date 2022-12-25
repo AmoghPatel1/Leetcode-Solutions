8134517e9aa14e23d3162b2ca8b7c7da79c04e7a
Amogh Patel <amogh.patel.2000@gmail.com>
2022-12-25 09:52:03 +0530

    Time: 13 ms (98.00%), Space: 13.8 MB (36.20%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int binary_search(vector<int> &nums, int num){
-        int left = 0, right = nums.size()-1;
-        while(left <= right) {
-            int mid = (left+right)/2;
-            if(nums[mid] == num) return mid+1;
-            else if(nums[mid] < num) left = mid+1;
-            else right = mid-1;
-        }
-        return left;
-    }
+    // int binary_search(vector<int> &nums, int num){
+    //     int left = 0, right = nums.size()-1;
+    //     while(left <= right) {
+    //         int mid = (left+right)/2;
+    //         if(nums[mid] == num) return mid+1;
+    //         else if(nums[mid] < num) left = mid+1;
+    //         else right = mid-1;
+    //     }
+    //     return left;
+    // }
     
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(), nums.end());
         for(int i=1;i<nums.size();i++)
             nums[i] += nums[i-1];
-        
-        // for(int i=0;i<nums.size();i++) {
-        //     cout << nums[i] << " ";
-        // }
-        // cout << endl;
         vector<int> v;
         for(int i=0;i<queries.size();i++) {
-            int index = binary_search(nums, queries[i]);
+            // int index = binary_search(nums, queries[i]);
+            int index = upper_bound(nums.begin(), nums.end(), queries[i]) - nums.begin();
             v.push_back(index);
         }
         return v;
