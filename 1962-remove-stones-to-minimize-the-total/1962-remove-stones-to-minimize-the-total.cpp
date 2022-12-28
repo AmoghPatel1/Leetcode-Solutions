70d999205a98912e79fc46801db630466f7f3665
Amogh Patel <amogh.patel.2000@gmail.com>
2022-12-28 09:51:33 +0530

    Time: 529 ms (94.48%), Space: 98.9 MB (84.14%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
+//     void showpq(priority_queue<int> gq) {
+//         priority_queue<int> g = gq;
+//         while (!g.empty()) {
+//             cout << '\t' << g.top();
+//             g.pop();
+//         }
+//         cout << '\n';
+//     }
+    
     int minStoneSum(vector<int>& piles, int k) {
         priority_queue<int> heap(piles.begin(), piles.end());
         int totalSum = accumulate(piles.begin(), piles.end(), 0);
-        
         for(int i=0;i<k;i++) {
+            // showpq(heap);
             int cur = heap.top();
             heap.pop();
             int remove = cur/2;
             totalSum -= remove;
             heap.push(cur-remove);
+            
         }
+        // showpq(heap);
         return totalSum;
     }
 };
