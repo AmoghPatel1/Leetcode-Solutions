08a8d9cf19f23f77acdca213504496ce0928387f
Amogh Patel <amogh.patel.2000@gmail.com>
2022-12-28 21:56:57 +0530

    Time: 2 ms (84.46%), Space: 7.2 MB (54.81%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
-    void f(int ind, vector<int> &ds, vector<int> &nums, vector<vector<int>> &subsets) {
+    int f(int ind, int var, vector<int> &nums) {
         if(ind == nums.size()){
-            if(ds.size() == 0) return;
-            subsets.push_back(ds);
-            return;
+            return var;
         }
-        ds.push_back(nums[ind]);
-        f(ind+1, ds, nums, subsets);
-        ds.pop_back();
-        f(ind+1, ds, nums, subsets);
+        int pick = f(ind+1, var^nums[ind], nums);
+        int notPick = f(ind+1, var, nums);
+        return pick+notPick;
     }
     int subsetXORSum(vector<int>& nums) {
-        vector<int> ds;
-        vector<vector<int>> subsets;
-        f(0, ds, nums, subsets);
-        int sum = 0;
-        for(int i=0;i<subsets.size();i++) {
-            int p = subsets[i][0];
-            for(int j=1;j<subsets[i].size();j++) {
-                p ^= subsets[i][j];
-            }
-            sum += p;
-        }
-        return sum;
+        return f(0, 0, nums);
     }
 };
