e77fa840e3bf66dbf689314edfc9ba97153eb5cc
Amogh Patel <amogh.patel.2000@gmail.com>
2023-01-06 11:35:03 +0530

    Time: 5 ms (51.56%), Space: 7.4 MB (98.88%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    void findPermutations(vector<int> &nums, vector<int> &ds, vector<bool> &freq, vector<vector<int>> &ans){
-        if(ds.size() == nums.size()) {
-            ans.push_back(ds);
+    void findPermutations(int ind, vector<int> &nums, vector<vector<int>> &ans){
+        if(ind == nums.size()) {
+            ans.push_back(nums);
             return; 
         }
-        for(int i=0;i<nums.size();i++) {
-            if(!freq[i]) {
-                ds.push_back(nums[i]);
-                freq[i] = true;
-                findPermutations(nums, ds, freq, ans);
-                ds.pop_back();
-                freq[i] = false;
-            }
+        for(int i=ind;i<nums.size();i++) {
+            swap(nums[i], nums[ind]);
+            findPermutations(ind+1, nums, ans);
+            swap(nums[i], nums[ind]);
         }
     }
     
+    // This works only when all the integers of nums array are unique.
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<bool> freq(nums.size(), false);
-        findPermutations(nums, ds, freq, ans);
+        findPermutations(0, nums, ans);
         return ans;
     }
 };
