3ac448c2bee1861f6e4427bca9e0b6a771a4d418
Amogh Patel <amogh.patel.2000@gmail.com>
2023-02-03 19:27:05 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (96.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,11 +1,8 @@
-class Solution
-{
+class Solution{
     public:
-        int rob(vector<int> &nums)
-        {
+        int rob(vector<int> &nums){
             int rob1 = 0, rob2 = 0;
-            for (int i = 0; i < nums.size(); i++)
-            {
+            for (int i = 0; i < nums.size(); i++){
                 int temp = max(rob1 + nums[i], rob2);
                 rob1 = rob2;
                 rob2 = temp;
