29ec6ce249de81b1d905c72e30e81afab657820a
Amogh Patel <amogh.patel.2000@gmail.com>
2023-02-13 21:19:09 +0530

    Time: 14 ms (34.29%), Space: 9.9 MB (74.81%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -12,21 +12,23 @@ public:
     
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n, 0));
+        // vector<vector<int>> dp(m, vector<int> (n, 0));
         // return recursion(m-1, n-1, grid, dp);
-        
+        vector<int> prev(n, 0);
         for(int i=0;i<m;i++){
+            vector<int> cur(n,0);
             for(int j=0;j<n;j++) {
-                if(i==0 && j==0) dp[i][j] = grid[0][0];
+                if(i==0 && j==0) cur[j] = grid[0][0];
                 else {
                     int up = INT_MAX, left = INT_MAX;
-                    if(i>0) up = grid[i][j] + dp[i-1][j];
-                    if(j>0) left = grid[i][j] + dp[i][j-1];
-                    dp[i][j] = min(up, left);
+                    if(i>0) up = grid[i][j] + prev[j];
+                    if(j>0) left = grid[i][j] + cur[j-1];
+                    cur[j] = min(up, left);
                 }
             }
+            prev = cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
         
     }
 };
