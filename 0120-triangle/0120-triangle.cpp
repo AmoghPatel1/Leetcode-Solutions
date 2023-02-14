12a6b4beca65040514bed62f93a7fbf9ec1fd8e9
Amogh Patel <amogh.patel.2000@gmail.com>
2023-02-14 23:38:23 +0530

    Time: 8 ms (52.42%), Space: 9.1 MB (12.16%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int recursion(int row, int col, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        if(row == triangle.size()-1) return triangle[row][col];
-        if(dp[row][col] != -1) return dp[row][col];
-        int down = triangle[row][col] + recursion(row+1, col, triangle, dp);
-        int diagonal = triangle[row][col] + recursion(row+1, col+1, triangle, dp);
-        return dp[row][col] = min(down, diagonal);
-    }
-    
+
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int> (n,-1));
-        return recursion(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int> (n,0));
+        for(int j=0;j<n;j++) {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        
+        for(int i=n-2;i>=0;i--) {
+            for(int j=i;j>=0;j--) {
+                int down = triangle[i][j] + dp[i+1][j];
+                int diagonal = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
 };
