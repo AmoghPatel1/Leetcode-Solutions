169fc16aecc4d6a259505b14b454535fe3041014
Amogh Patel <amogh.patel.2000@gmail.com>
2023-02-15 11:14:49 +0530

    Time: 23 ms (21.70%), Space: 10.3 MB (57.27%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int dfs(vector<vector<int>>& matrix , int i , int j){
-        if(j >= matrix.size() || i>=matrix.size() || j<0){
-            if(i >= matrix.size())return 0;
-            return 1e6;
-        }
-        if(dp[i][j]!=1e9)return dp[i][j];
-        return dp[i][j] = matrix[i][j] + min({dfs(matrix , i + 1 , j - 1) , dfs(matrix , i + 1 , j),
-        dfs(matrix , i + 1 , j + 1)}); 
+    int recursion(int row, int col, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        if(row == matrix.size()-1) return matrix[row][col];
+        if(dp[row][col] != -1) return dp[row][col];
+        int down = matrix[row][col] + recursion(row+1, col, matrix, dp);
+        int downLeft = INT_MAX, downRight = INT_MAX;
+        if(col>0) downLeft = matrix[row][col] + recursion(row+1, col-1, matrix, dp);
+        if(col<matrix.size()-1) downRight = matrix[row][col] + recursion(row+1, col+1, matrix, dp);
+        return dp[row][col] = min(min(down, downLeft), min(down, downRight));
+        
     }
-
+    
     int minFallingPathSum(vector<vector<int>>& matrix) {
-       int ans = INT_MAX;
-       int n = matrix.size();
-        dp.assign(n, vector<int>(n , 1e9));
-
-       for(int i = 0; i < matrix.size(); i++){
-           ans = min(dfs(matrix , 0 , i) , ans);
-       }
-       return ans;
+        int n = matrix.size();
+        vector<vector<int>> dp(n, vector<int> (n, -1));
+        int ans = INT_MAX;
+        for(int i=0;i<n;i++) {
+            ans = min(ans, recursion(0, i, matrix, dp));
+        }
+        return ans;
     }
 };
