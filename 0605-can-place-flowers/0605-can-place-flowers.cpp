ef7b75dec3f98bd7775f6c4a8dc2a1896dfe332c
Amogh Patel <amogh.patel.2000@gmail.com>
2023-03-20 23:18:58 +0530

    Time: 20 ms (53.38%), Space: 20.3 MB (76.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int m) {
-        if(m == 0) return true;
-        int count = 0, n = flowerbed.size();
-        if(n == 1 && m == 1) {
-            if(flowerbed[0] == 0)return true;
-            else return false;
-        }
-        for(int i=0;i<n;i++) {
-            if(i==0 && flowerbed[i]==0 && i+1<n && flowerbed[i+1]==0) {
-                count += 1;
-                flowerbed[i] = 1;
-            } else if(i==n-1 && flowerbed[i-1] == 0 && flowerbed[i] == 0){
-                count += 1;
-                flowerbed[i] = 1;
-            } else {
-                if(flowerbed[i] == 0 && i+1<n && flowerbed[i+1] == 0 && flowerbed[i-1] == 0) {
-                    count += 1;
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); i++) {
+            // Check if the current plot is empty.
+            if (flowerbed[i] == 0) {
+                // Check if the left and right plots are empty.
+                bool emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                bool emptyRightPlot = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
+                
+                // If both plots are empty, we can plant a flower here.
+                if (emptyLeftPlot && emptyRightPlot) {
                     flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) {
+                        return true;
+                    }
                 }
             }
         }
-        if(count >= m) return true;
-        return false;
+        return count >= n;
     }
 };
