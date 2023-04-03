ada53bf47f85b030ff83d9cea1dec8ed8d15fb09
Amogh Patel <amogh.patel.2000@gmail.com>
2023-04-03 23:45:03 +0530

    Time: 90 ms (52.24%), Space: 41.9 MB (59.84%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        sort(people.begin(),people.end());
+        int left = 0, right = n-1, ans = 0;
+        int maxm = -1;
+        while(left < right) {
+            if(people[left] + people[right] <= limit) {
+                maxm = max(right, maxm);
+                right -= 1;
+                left += 1;
+                ans += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        ans = n - ans;
+        return ans;
+    }
+};
