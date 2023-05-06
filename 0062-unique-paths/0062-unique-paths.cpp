9dad7b43954cfa0263d2e2b58a1665c2b903ad06
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-06 21:36:33 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.30%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    
-//     int recursion(int row, int col, vector<vector<int>> &dp) {
-//         if(row == 0 && col == 0)
-//             return 1;
-//         if(row < 0 || col < 0)
-//             return 0;
-//         if(dp[row][col] != -1) return dp[row][col];
-//         int up = recursion(row-1, col, dp);
-//         int left = recursion(row, col-1, dp);
-        
-//         return dp[row][col] = up + left;
-//     }
+    int solve(int i, int j, int m, int n, vector<vector<int>>& dp) {         // Time Complexity = O(2^n). i.e. Exponential.
+        if(i==m || j == n) return 0;                // Space Complexity = O(2^n). i.e. Exponential.  (Recursion Stack space)
+        if(i==m-1 && j==n-1) {
+            return 1;
+        } 
+        if(dp[i][j] != -1) return dp[i][j];
+        int down = solve(i+1, j, m, n, dp);
+        int right = solve(i, j+1, m, n, dp);
+        return dp[i][j] = down + right;
+    }
     
     int uniquePaths(int m, int n) {
-        vector<int> dp(n,1);
-        for(int i=1;i<m;i++) {
-            vector<int> temp(n,0);
-            for(int j=0;j<n;j++) {
-                int up = 0, left = 0;
-                if(i>0) up = dp[j];
-                if(j>0) left = temp[j-1];
-                temp[j] = up + left;
-            }
-            dp = temp;
-        }
-        return dp[n-1];
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return solve(0, 0, m, n, dp);
     }
 };
