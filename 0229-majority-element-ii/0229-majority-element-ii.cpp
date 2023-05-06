f751a7c9d6f8995ef4aa5689782c05eb49a0424a
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-06 12:25:51 +0530

    Time: 11 ms (84.54%), Space: 16 MB (18.26%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,28 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        map<int,int> mp;
-        for(auto it: nums){
-            mp[it] += 1;
+        int can1 = -1, can2 = -1, count1 = 0, count2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == can1) count1 += 1;
+            else if(nums[i] == can2) count2 += 1;
+            else if(count1 == 0) {
+                can1 = nums[i];
+                count1 = 1;
+            } else if(count2 == 0) {
+                can2 = nums[i];
+                count2 = 1;
+            } else {
+                count1 -= 1; count2 -= 1;
+            }
+        }
+        count1 = count2 = 0;
+        for(int i=0;i<n;i++) {
+            if(nums[i] == can1) count1 += 1;
+            else if(nums[i] == can2) count2 += 1;
         }
         vector<int> ans;
-        for(auto it: mp) {
-            if(it.second > n/3) ans.push_back(it.first);
-        }
+        if(count1 > n/3) ans.push_back(can1);
+        if(count2 > n/3) ans.push_back(can2);
         return ans;
     }
 };
