fd2458dbac4c7a7cca4aab3eb42a8c63777e24cc
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-07 11:07:29 +0530

    Time: 78 ms (8.40%), Space: 44.4 MB (87.81%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -5,9 +5,9 @@
 var createCounter = function(init) {
     let val = init;
     return {
-        "increment": function () { return ++val; },
-        "reset": function () { val = init; return val; },
-        "decrement": function() { return --val; }
+        increment: function () { return ++val; },
+        reset: function () { val = init; return val; },
+        decrement: function() { return --val; }
     }
 };
 
