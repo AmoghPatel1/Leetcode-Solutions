9ef14c3ad9115a6fe4608c83a2fdb4b6d4ad5b7b
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-08 11:48:48 +0530

    Time: 15 ms (56.74%), Space: 11.3 MB (64.67%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,8 +4,6 @@ public:
         int n = mat.size(), sum = 0;
         for(int i=0;i<n;i++) {
             sum += mat[i][i];
-        }
-        for(int i=0;i<n;i++) {
             sum += mat[i][n-1-i];
         }
         return n%2==0 ? sum : sum - mat[n/2][n/2];
