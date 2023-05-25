951331a8a214acaab6c2074320055465c82e1e41
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-25 13:44:43 +0530

    Time: 8 ms (28.34%), Space: 7.6 MB (58.36%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    void fun(int index, vector<int> nums, vector<int> v, set<vector<int>> &s) {
-        if(index == nums.size()) {
-            s.insert(v);   
-            return;
-        }
-        fun(index+1, nums, v, s);
-        v.push_back(nums[index]);
-        fun(index+1, nums, v, s);    
+//     void fun(int index, vector<int> nums, vector<int> v, vector<vector<int>> &ans) {
+//         ans.push_back(v);
         
-    }
+//         for(int i=index;i<nums.size();i++) {
+//             if(i>index && nums[i] == nums[i-1]) continue;
+//             v.push_back(nums[i]);
+//             fun(i+1, nums, v, ans);
+//             v.pop_back();
+//         }
+//     }
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> s;\
         sort(nums.begin(), nums.end());
-        fun(0, nums, {}, s);
-        vector<vector<int>> ans;
-        for(auto it: s) ans.push_back(it);
-        return ans; 
+
+        vector<vector<int>> results;
+
+        function<void(int, vector<int>)> recurse = [&](int index, vector<int> subset) {
+            results.push_back(subset);
+            for(int i=index;i<nums.size();i++) {
+                if(i>index && nums[i] == nums[i-1]) continue;
+                subset.push_back(nums[i]);
+                recurse(i+1, subset);
+                subset.pop_back();
+            } 
+        };
+
+        recurse(0, {});
+
+        return results;
     }
 };
