f312ecfec2e613d2e9fba2e2e22edd48c0f9c380
Amogh Patel <amogh.patel.2000@gmail.com>
2023-05-25 18:45:24 +0530

    Time: 114 ms (11.00%), Space: 31.1 MB (21.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    // int count = 0;
-    void f(vector<vector<int>> &v, int index, vector<int> &ds, vector<int> &candidates, int target) {
-        // for(int i=0;i<ds.size();i++) cout << ++count << " ";
-        cout << endl;
-        if(index == candidates.size()) {
-            if(target == 0) {
-                v.push_back(ds);
-            }
-            return;
-        }
-        if(candidates[index] <= target) {
-            ds.push_back(candidates[index]);
-            f(v, index, ds, candidates, target-candidates[index]);
-            ds.pop_back();
-        }
-        f(v, index+1, ds, candidates, target);
-    }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> v;
-        vector<int> ds;
-        f(v, 0, ds, candidates, target);
-        return v;
+        vector<vector<int>> ans; 
+        int n = candidates.size();
+        function<void(int, int, vector<int>)> recurse = [&] (int ind, int sum, vector<int> comb) {
+            if(ind >= n || sum >= target) {
+                if(sum == target) {
+                    ans.push_back(comb);
+                } 
+                return;
+            }
+            recurse(ind+1, sum, comb);
+            sum += candidates[ind];
+            comb.push_back(candidates[ind]);
+            recurse(ind, sum, comb);      
+        };
+        recurse(0, 0, {});
+        return ans;
     }
 };
