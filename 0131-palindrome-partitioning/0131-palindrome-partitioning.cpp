33153e1e13c33c5abbc2d18459ea8734154e2d3e
Amogh Patel <amogh.patel.2000@gmail.com>
2023-06-08 16:34:11 +0530

    Time: 383 ms (5.04%), Space: 140.7 MB (16.22%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool isPalindrome(string s, int start, int end) {
-        while(start <= end) {
-            if(s[start] != s[end]) return false;
-            start+=1; end-=1;
+    bool isPalindrome(string str) {
+        int n = str.size();
+        for(int i=0;i<n/2;i++) {
+            if(str[i] != str[n-i-1]) return false;
         }
+        cout << str << endl;
         return true;
-    }
+    } 
     
-    void func(int ind, string s, vector<string> &ds, vector<vector<string>> &ans) {
-        if(ind == s.size()){
-            ans.push_back(ds);
+    
+    void recurse(int index, string s, vector<string> v, vector<vector<string>> &ans){
+        if(index == s.size()) {
+            ans.push_back(v);
             return;
         }
-        for(int i=ind;i<s.size();i++) {
-            // string str = s.substr(ind, i-ind+1);
-            if(isPalindrome(s, ind, i)){
-                ds.push_back(s.substr(ind, i-ind+1));
-                func(i+1, s, ds, ans);
-                ds.pop_back();
+        for(int i=index+1;i<=s.size();i++) {
+            string str = s.substr(index, i-index);
+            if(isPalindrome(str)) {
+                v.push_back(str);
+                recurse(i, s, v, ans);
+                v.pop_back();
             }
-            
         }
     }
     
     vector<vector<string>> partition(string s) {
-        int n = s.size();
         vector<vector<string>> ans;
-        vector<string> ds;
-        func(0, s, ds, ans);
+        int n = s.size();
+        recurse(0, s, {}, ans);
         return ans;
     }
 };
