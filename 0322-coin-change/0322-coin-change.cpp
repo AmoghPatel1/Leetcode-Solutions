8e2c86fc9035f654cf5cb47b50671c3e848afe68
Amogh Patel <amogh.patel.2000@gmail.com>
2023-08-22 10:40:47 +0530

    Time: 81 ms (71.33%), Space: 14.5 MB (62.44%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -5,9 +5,7 @@ class Solution {
 public:
     int recurse(int amount, vector<int>&coins, vector<int> &dp) {
         if(amount == 0) return 0;
-        
         if(dp[amount] != -1) return dp[amount];
-        
         int ans = INF;
         for(int i=0;i<coins.size();i++) {
             int dif = amount - coins[i];
@@ -24,15 +22,23 @@ public:
                 }
             }
         }
-        
         return dp[amount] = ans;
     }
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount+1, -1);
+        // vector<int> dp(amount+1, -1);
+        // int ans = recurse(amount, coins, dp);
+
+        vector<int> dp(amount+1, INF);
         dp[0] = 0;
-        int ans = recurse(amount, coins, dp);
+        for(int i=1;i<=amount;i++) {
+            for(int j=0;j<n;j++) {
+                if(i-coins[j] >= 0)
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
+            }
+        }
+        int ans = dp[amount];
         return ans == INF ? -1 : ans;
     }
 };
