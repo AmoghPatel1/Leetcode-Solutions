30d50de0dcfe971232a89c5340e966acf41182c8
Amogh Patel <amogh.patel.2000@gmail.com>
2023-10-02 00:09:28 +0530

    Time: 43 ms (100%), Space: 45 MB (57.14%) - LeetHub

---
+++ 3141-minimum-size-subarray-in-infinite-array/3141-minimum-size-subarray-in-infinite-array.cpp
@@ -0,0 +1,30 @@
+#define pv(v) {for(int i=0;i<v.size();i++) {cout << v[i] << " ";}cout << endl;}
+class Solution {
+public:
+    int minSizeSubarray(vector<int>& nums, int target) {
+        int ans = 0;
+        int n = nums.size();
+        long long sum = accumulate(nums.begin(), nums.end(), 0*1LL);
+        if(target > sum) {
+            ans += (target/sum) * n;
+            target %= sum;
+        }
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        // cout << target << endl;
+        int count = INT_MAX;
+        int left = 0, right = 0;
+        long long cur = 0;
+        while(right < nums.size()) {
+            if(cur == target) count = min(count, right-left);
+            cur += nums[right];
+            // cout << left << " " << right << " " << cur << endl;
+            while(cur > target) {
+                cur -= nums[left];
+                left += 1;
+            }
+            // cout << count << endl;
+            right += 1;
+        }
+        return count != INT_MAX ? ans + count : -1;
+    }
+};
