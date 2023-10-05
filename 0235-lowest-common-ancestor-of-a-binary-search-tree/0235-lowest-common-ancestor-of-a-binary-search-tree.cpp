ead57090762827fff8339fae44e74ef0bfeb1d1a
Amogh Patel <amogh.patel.2000@gmail.com>
2023-10-05 18:55:30 +0530

    Time: 120 ms (5.59%), Space: 73.6 MB (5.19%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> first, second;
+
+    void inorder(TreeNode* root, TreeNode* p, TreeNode* q, vector<int> v, map<int, TreeNode*> &mp) {
+        if(!root) return;
+        if(root == p) {
+            v.push_back(root->val);
+            first.assign(v.begin(), v.end());
+        } else if(root == q) {
+            v.push_back(root->val);
+            second.assign(v.begin(), v.end());
+        }
+        mp[root->val] = root;
+        v.push_back(root->val);
+        inorder(root->left,p,q,v, mp);
+        inorder(root->right,p,q,v, mp);
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        map<int, TreeNode*> mp;
+        inorder(root, p, q, {}, mp);
+        int ans = INT_MAX;
+        set<int> s;
+        
+        for(int x : first) cout << x << " ";
+        cout << endl;
+
+        for(int x : second) cout << x << " ";
+        cout << endl;
+
+        for(auto x : first) s.insert(x);
+        for(auto x : second)  {
+            if(s.find(x) != s.end()) {
+                if(ans != INT_MAX && x < ans) ans = x;
+                else ans = x;
+            }
+        }
+        return mp[ans];
+    }
+};
