12867ae2d564c8a72abb78ddf0796afcd2c23f5c
Amogh Patel <amogh.patel.2000@gmail.com>
2024-07-19 21:30:14 +0530

    Time: 51 ms (25.22%), Space: 47.5 MB (23.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,44 +1,26 @@
 class Solution {
-
-#define INF 1e5
-    
 public:
-    int recurse(int amount, vector<int>&coins, vector<int> &dp) {
-        if(amount == 0) return 0;
-        if(dp[amount] != -1) return dp[amount];
-        int ans = INF;
-        for(int i=0;i<coins.size();i++) {
-            int dif = amount - coins[i];
-            if(dif >= 0) {
-                int subAns = 0;
-                
-                if(dp[dif] != -1)
-                    subAns = dp[dif];
-                else
-                    subAns = recurse(dif, coins, dp);
-                
-                if(subAns != INF && subAns+1 < ans){    
-                    ans = subAns + 1;
-                }
-            }
+    int fun(int x, vector<int>& coins, int amount, vector<vector<int>> &dp) {
+        if(amount < 0 || x < 0) return 1e9;
+        if(x == 0 && amount%coins[x] == 0) return amount/coins[x];
+
+        if(dp[x][amount] != -1) return dp[x][amount];
+        // not pick 
+        int notPick = fun(x-1, coins, amount, dp);
+
+        // pick
+        int pick = 1e9;
+        if(coins[x] <= amount) {
+            pick = 1 + fun(x, coins, amount-coins[x], dp);
         }
-        return dp[amount] = ans;
+        
+        return dp[x][amount] = min(pick, notPick);
     }
-    
+
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<int> dp(amount+1, -1);
-        // int ans = recurse(amount, coins, dp);
-
-        vector<int> dp(amount+1, INF);
-        dp[0] = 0;
-        for(int i=1;i<=amount;i++) {
-            for(int j=0;j<n;j++) {
-                if(i-coins[j] >= 0)
-                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
-            }
-        }
-        int ans = dp[amount];
-        return ans == INF ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = fun(n-1, coins, amount, dp);
+        return ans >= 1e9 ? -1 : ans;
     }
 };
